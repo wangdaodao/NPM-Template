50a4bbf98fa89469719ee959c902a03d02672ee4
王叨叨 <hi@wangdaodao.com>
2021-01-11 11:18:39 +0800

    add：jest test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  'collectCoverage': true,
+  'collectCoverageFrom': [
+    'src/**/*.vue',
+    '!src/main.js',
+    '!src/App.vue',
+    '!**/node_modules/**'
+  ]
 }
