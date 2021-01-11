15435aa90692abcb2e33717ad052555de2080787
王叨叨 <hi@wangdaodao.com>
2021-01-11 11:02:46 +0800

    add：jest test

--- tests/unit/hello-world.spec.js
+++ tests/unit/hello-world.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/hello-world.vue'
 
 describe('<hello-world/>', () => {
-  it('should render correct contents', () => {
+  test('should render correct contents', () => {
     const wrapper = shallowMount(HelloWorld);
     expect(wrapper.find('h1').text())
       .toEqual('Hello Jest');
