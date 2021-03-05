f6abdf6e5ea2dc17412f375bdae388fc24f8a3e8
王叨叨 <hi@wangdaodao.com>
2021-03-05 13:18:14 +0800

    更新物料模板

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      "component",
+      { "libraryName": "materiel-demo", "style": false, },
+      "materiel-demo"
+    ]
   ]
 }
